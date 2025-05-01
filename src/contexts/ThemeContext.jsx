79250f28fd583648be3769964eba724cd979778a
ligningzehra <zhrasmsk1999@gmail.com>
2025-05-02 00:47:06 +0300

    themecontext

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "light";
+  });
+
+  useEffect(() => {
+    const element = document.documentElement;
+    if (theme === "dark") {
+      element.classList.add("dark");
+      element.classList.remove("light");
+    } else {
+      element.classList.remove("dark");
+      element.classList.add("light");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  // Tema değiştirme fonk.
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
