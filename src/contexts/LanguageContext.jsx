d3402a5ecee0f6a6f77a85989f3322a7329b697f
ligningzehra <zhrasmsk1999@gmail.com>
2025-05-02 00:53:18 +0300

    languagecontext

---
+++ src/contexts/LanguageContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+
+const LanguageContext = createContext();
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState("en");
+
+  const toggleLanguage = () => {
+    setLanguage(language === "en" ? "tr" : "en");
+  };
+
+  const changeLanguage = (newLanguage) => {
+    setLanguage(newLanguage);
+  };
+
+  return (
+    <LanguageContext.Provider
+      value={{ language, toggleLanguage, changeLanguage }}
+    >
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => useContext(LanguageContext);
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { LanguageProvider } from "./contexts/LanguageContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <LanguageProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </LanguageProvider>
   </React.StrictMode>
 );
